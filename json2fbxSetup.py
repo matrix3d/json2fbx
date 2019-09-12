c3dfdbc5ebd93a7c244134db3757553374964104
matrix3d <sliz@qq.com>
2019-09-12 16:26:06 +0800

    gen exe

---
+++ json2fbxSetup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+import py2exe
+import sys
+
+sys.argv.append('py2exe')
+INCLUDES = ["fbxsip"]
+
+options = {
+    "py2exe" :
+        {
+            "compressed" : 1, # 压缩   
+            "optimize" : 2,
+            "bundle_files" : 1, # 所有文件打包成一个 exe 文件  
+            "includes" : INCLUDES,
+            "dll_excludes" : ["MSVCR100.dll"]
+        }
+}
+
+setup(
+    options=options,    
+    description = "this is a py2exe test",   
+    zipfile=None,
+    #console = [{"script":'Json2fbx.py'}])
+    console = [{"script":'Json2fbx.py',"icon_resources":[(1,"favicon.ico")]}])
